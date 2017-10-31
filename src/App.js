efd703b932cbb0d2bc14d6f2ac38d84f7bffea50
Lukas Gisder-Dubé <gisderdube@gmail.com>
2017-10-31 15:11:45 +0100

    setup for diego

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 
-import SearchBar from './search-bar';
+import MemberList from './MemberList';
 
 const StyledWrapper = styled.div`
     width: 100vw;
-    height: 100vh;
+    min-height: 100vh;
+    padding-top: 100px;
     position: relative;
     z-index: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
-const StyledMask = styled.div`
-    background-color: #FFF;
-    display: block;
-    width: 100%;
-    height: 50vh;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-
-    &::after {
-        background-color: #7E57C2;
-        content: '';
-        display: block;
-        width: 100%;
-        height: 50vh;
-        position: absolute;
-        top: 50vh;
-        z-index: -1;
-    }
+    background: linear-gradient(135deg, #c917a0 0%,#7e57c2 100%);
 `;
 
 const StyledLogoLink = styled.a`
@@ -49,11 +25,10 @@ export default class App extends Component {
     render() {
         return (
             <StyledWrapper>
-                <StyledMask />
                 <StyledLogoLink href='https://interclub.io' target='_blank'>
-                    <img src='/assets/logo_48x48.png' />
+                    <img src='/assets/inv_logo_48x48.png' />
                 </StyledLogoLink>
-                <SearchBar />
+                <MemberList />
             </StyledWrapper>
         );
     }
